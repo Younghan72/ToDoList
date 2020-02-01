59d0ed8963006f588eceab09398e44a507f08bb4
Paul <kimyoungmin77677279@icloud.com>
2020-02-01 10:56:22 +0900

    Click Event Add

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
+var data = [
+    {
+        id:1,
+        content: '할일1',
+        do:true
+    }
+    ,
+    {
+        id: 2,
+        content: '할일2',
+        do: true
+    }
+    ,
+    {
+        id: 3,
+        content: '할일3',
+        do: false
+    }
+]
 document.getElementById("add").addEventListener('click', addButtonClick)
 var list = document.getElementById('list')
 function addButtonClick() {
@@ -16,15 +35,25 @@ function addButtonClick() {
     label.appendChild(value)
     div.appendChild(i)
 
-    label.classList.add("checked")
+    // label.classList.add("checked")
     i.classList.add("far","fa-trash-alt")
 
     list.appendChild(div)
 
     contents.value = ""
-
     i.addEventListener('click', remove)
+
+    label.addEventListener('click', checkLine)
+    
+   
 }
 function remove(){
     list.removeChild(event.target.parentNode)
+}
+function checkLine(event) {
+       if (event.target.checked) {
+           event.target.parentNode.classList.add("checked")
+       } else{
+           event.target.parentNode.classList.remove('checked')
+       }
 }
