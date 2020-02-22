e2554b0bb275c2a6d2796733d1d2e48e8e311c19
Paul <kimyoungmin77677279@icloud.com>
2020-02-22 10:14:51 +0900

    데이터 삭제/수정 처리개선

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 var data = [
     {
-        id:1,
+        id: 4,
         content: '할일1',
         do:true
     }
     ,
     {
-        id: 2,
+        id: 6,
         content: '할일2',
         do: true
     }
     ,
     {
-        id: 3,
+        id: 5,
         content: '할일3',
         do: false
     }
@@ -23,28 +23,35 @@ for (let x = 0; x < data.length; x++) {
 }
 function makingList(target) {
     console.log(target)
-        var div = document.createElement('div')
-        var label = document.createElement('label')
-        var input = document.createElement('input')
-        var i = document.createElement('i')
-        var value = document.createTextNode(target.content)
+    var div = document.createElement('div')
+    var label = document.createElement('label')
+    var input = document.createElement('input')
+    var i = document.createElement('i')
+    var value = document.createTextNode(target.content)
 
-        input.type = "checkbox"
-        div.id = target.id
-        div.appendChild(label)
-        label.appendChild(input)
-        label.appendChild(value)
-        div.appendChild(i)
+    input.type = "checkbox"
+    div.id = target.id
+    div.appendChild(label)
+    label.appendChild(input)
+    label.appendChild(value)
+    div.appendChild(i)
+    
+    //배열의 DO 값이 True 면 Checked 클래스 주기
+    if (target.do==1) {
+        label.classList.add("checked")
+        input.checked=true
 
-        // label.classList.add("checked")
-        i.classList.add("far", "fa-trash-alt")
+    }
 
-        list.appendChild(div)
+    
+    i.classList.add("far", "fa-trash-alt")
+
+    list.appendChild(div)
 
 
-        i.addEventListener('click', remove)
+    i.addEventListener('click', remove)
 
-        label.addEventListener('click', checkLine)
+    label.addEventListener('click', checkLine)
 }
 
 document.getElementById("add").addEventListener('click', addButtonClick)
@@ -64,14 +71,33 @@ function addButtonClick() {
     makingList(data[data.length-1])
     contents.value = ""
 }
+
 function remove(){
     list.removeChild(event.target.parentNode)
-    data.splice(event.target.parentNode.id-1,1)
+    console.log(event.target.parentNode.id)
+
+//  es5  var index = data.findIndex(function(object){
+//        return object.id == event.target.parentNode.id
+//    })
+
+    //es6
+    var index = data.findIndex(object => object.id == event.target.parentNode.id)
+    console.log(index)
+    data.splice(index,1)
 }
+//checked==true data.do=true
 function checkLine(event) {
-       if (event.target.checked) {
-           event.target.parentNode.classList.add("checked")
-       } else{
-           event.target.parentNode.classList.remove('checked')
-       }
+    if (event.target.tagName == "LABEL") {
+        return false
+    }
+    
+    var index = data.findIndex(object => object.id == event.target.parentNode.parentNode.id)
+    console.log(index)
+    if (event.target.checked) {
+        event.target.parentNode.classList.add("checked")
+        data[index].do=true
+    } else{
+        event.target.parentNode.classList.remove('checked')
+        data[index].do = false
+    }
 }
