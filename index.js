46519fb02e242273bed23a022cbee4fe1423c087
Paul <kimyoungmin77677279@icloud.com>
2020-04-12 13:21:32 +0900

    uP

--- index.js
+++ index.js
@@ -1,33 +1,33 @@
-var data = [
-    {
-        id: 4,
-        content: '할일1',
-        do:true
-    }
-    ,
-    {
-        id: 6,
-        content: '할일2',
-        do: true
-    }
-    ,
-    {
-        id: 5,
-        content: '할일3',
-        do: false
-    }
-]
 var list = document.getElementById('list')
-for (let x = 0; x < data.length; x++) {
-    makingList(data[x])
+var data
+function getData() {
+    fetch('https://jsonplaceholder.typicode.com/todos?userId=1')
+        .then(res =>{
+            if(res.status==200){
+                return res.json()
+            } else {
+                console.log(res.statusText)
+            }
+        })
+        .then(jsonData =>{
+            data = jsonData
+            for (let i = 0; i < jsonData.length; i++) {
+                makingList(jsonData[i])
+                
+            }
+        })
+        .catch(err =>{
+            console.log(err)
+        })
 }
+getData()
 function makingList(target) {
-    console.log(target)
+    // console.log(target)
     var div = document.createElement('div')
     var label = document.createElement('label')
     var input = document.createElement('input')
     var i = document.createElement('i')
-    var value = document.createTextNode(target.content)
+    var value = document.createTextNode(target.title)
 
     input.type = "checkbox"
     div.id = target.id
@@ -37,7 +37,7 @@ function makingList(target) {
     div.appendChild(i)
     
     //배열의 DO 값이 True 면 Checked 클래스 주기
-    if (target.do==1) {
+    if (target.completed==1) {
         label.classList.add("checked")
         input.checked=true
 
@@ -64,8 +64,8 @@ function addButtonClick() {
     data.push(
         {
             id: data.length+1 ,
-            content: contents.value ,
-            do: false
+            title: contents.value ,
+            completed: false
         }
     )
     makingList(data[data.length-1])
@@ -74,30 +74,16 @@ function addButtonClick() {
 
 function remove(){
     list.removeChild(event.target.parentNode)
-    console.log(event.target.parentNode.id)
-
-//  es5  var index = data.findIndex(function(object){
-//        return object.id == event.target.parentNode.id
-//    })
-
-    //es6
-    var index = data.findIndex(object => object.id == event.target.parentNode.id)
-    console.log(index)
-    data.splice(index,1)
 }
-//checked==true data.do=true
+//checked==true data.completed=true
 function checkLine(event) {
     if (event.target.tagName == "LABEL") {
         return false
     }
     
-    var index = data.findIndex(object => object.id == event.target.parentNode.parentNode.id)
-    console.log(index)
     if (event.target.checked) {
         event.target.parentNode.classList.add("checked")
-        data[index].do=true
     } else{
         event.target.parentNode.classList.remove('checked')
-        data[index].do = false
     }
 }
