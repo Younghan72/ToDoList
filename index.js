7d7a29ac6d5a0ba74d90204656e7cf7b1f1e0460
dudals4657 <kimyoungmin77677279@icloud.com>
2020-04-26 14:09:23 +0900

    !finish!

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 var list = document.getElementById('list')
 var data
 function getData() {
-    fetch('https://jsonplaceholder.typicode.com/todos?userId=1')
+    document.getElementById('list').innerHTML=""
+    fetch('http://127.0.0.1:3000/todos')
         .then(res =>{
             if(res.status==200){
                 return res.json()
@@ -19,6 +20,7 @@ function getData() {
         .catch(err =>{
             console.log(err)
         })
+        
 }
 getData()
 function makingList(target) {
@@ -58,32 +60,64 @@ document.getElementById("add").addEventListener('click', addButtonClick)
 
 function addButtonClick() {
     var contents = document.getElementById("text")
-
+    fetch(`http://127.0.0.1:3000/todos?title=${text.value}`, {
+        method: 'post'
+    })
+    .then(res =>{
+        if(res.status==200){
+            //get Data 함수 다시 호출
+            getData()
+        } 
+    })
+    .catch(err =>{
+        console.log(err)
+    })
     //배열의 길이 가져오기(배열이름.length)
     //배열에 값 추가하기
-    data.push(
-        {
-            id: data.length+1 ,
-            title: contents.value ,
-            completed: false
-        }
-    )
-    makingList(data[data.length-1])
     contents.value = ""
+    
 }
 
 function remove(){
-    list.removeChild(event.target.parentNode)
+    fetch(`http://127.0.0.1:3000/todos?id=${event.target.parentNode.id}`, {
+        method: 'delete'
+    })
+    .then(res =>{
+        if(res.status==200){
+            //get Data 함수 다시 호출
+            getData()
+        } 
+    })
+    .catch(err =>{
+        console.log(err)
+    })
 }
 //checked==true data.completed=true
 function checkLine(event) {
+    var check
     if (event.target.tagName == "LABEL") {
         return false
     }
-    
-    if (event.target.checked) {
-        event.target.parentNode.classList.add("checked")
-    } else{
-        event.target.parentNode.classList.remove('checked')
+    if (event.target.checked){
+        check = 1
+    } else {
+        check = 0
     }
+    fetch(`http://127.0.0.1:3000/todos?id=${event.target.parentNode.parentNode.id}&completed=${check}`, {
+        method: 'put'
+    })
+    .then(res =>{
+        if(res.status==200){
+            //get Data 함수 다시 호출
+            getData()
+        } 
+    })
+    .catch(err =>{
+        console.log(err)
+    })
+    // if (event.target.checked) {
+    //     event.target.parentNode.classList.add("checked")
+    // } else{
+    //     event.target.parentNode.classList.remove('checked')
+    // }
 }
